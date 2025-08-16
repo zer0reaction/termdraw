1e949a7c2ce19656b9b32354e9971e8dc977deab
zer0reaction <boltgopnik@gmail.com>
2025-08-17 00:18:00 +0300

    Reworked modes/styles

--- termdraw.h
+++ termdraw.h
@@ -17,10 +17,63 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 
+#define TERMDRAW_MODE_BOLD          1
+#define TERMDRAW_MODE_DIM           2
+#define TERMDRAW_MODE_ITALIC        3
+#define TERMDRAW_MODE_UNDERLINE     4
+#define TERMDRAW_MODE_BLINKING      5
+#define TERMDRAW_MODE_REVERSE       7
+#define TERMDRAW_MODE_INVISIBLE     8
+#define TERMDRAW_MODE_STRIKETHROUGH 9
 
-typedef uint32_t termdraw_rune;
+#define TERMDRAW_MODE_FG_BLACK   30
+#define TERMDRAW_MODE_FG_RED     31
+#define TERMDRAW_MODE_FG_GREEN   32
+#define TERMDRAW_MODE_FG_YELLOW  33
+#define TERMDRAW_MODE_FG_BLUE    34
+#define TERMDRAW_MODE_FG_MAGENTA 35
+#define TERMDRAW_MODE_FG_CYAN    36
+#define TERMDRAW_MODE_FG_WHITE   37
+#define TERMDRAW_MODE_FG_DEFAULT 39
 
-extern int  termdraw_initialize(void);
+#define TERMDRAW_MODE_BG_BLACK   40
+#define TERMDRAW_MODE_BG_RED     41
+#define TERMDRAW_MODE_BG_GREEN   42
+#define TERMDRAW_MODE_BG_YELLOW  43
+#define TERMDRAW_MODE_BG_BLUE    44
+#define TERMDRAW_MODE_BG_MAGENTA 45
+#define TERMDRAW_MODE_BG_CYAN    46
+#define TERMDRAW_MODE_BG_WHITE   47
+#define TERMDRAW_MODE_BG_DEFAULT 49
+
+#define TERMDRAW_MODE_FG_BRIGHT_BLACK   90
+#define TERMDRAW_MODE_FG_BRIGHT_RED     91
+#define TERMDRAW_MODE_FG_BRIGHT_GREEN   92
+#define TERMDRAW_MODE_FG_BRIGHT_YELLOW  93
+#define TERMDRAW_MODE_FG_BRIGHT_BLUE    94
+#define TERMDRAW_MODE_FG_BRIGHT_MAGENTA 95
+#define TERMDRAW_MODE_FG_BRIGHT_CYAN    96
+#define TERMDRAW_MODE_FG_BRIGHT_WHITE   97
+
+#define TERMDRAW_MODE_BG_BRIGHT_BLACK   100
+#define TERMDRAW_MODE_BG_BRIGHT_RED     101
+#define TERMDRAW_MODE_BG_BRIGHT_GREEN   102
+#define TERMDRAW_MODE_BG_BRIGHT_YELLOW  103
+#define TERMDRAW_MODE_BG_BRIGHT_BLUE    104
+#define TERMDRAW_MODE_BG_BRIGHT_MAGENTA 105
+#define TERMDRAW_MODE_BG_BRIGHT_CYAN    106
+#define TERMDRAW_MODE_BG_BRIGHT_WHITE   107
+
+typedef uint32_t termdraw_rune_t;
+
+typedef struct
+{
+    bool raw_mode;
+} termdraw__options_initialize_t;
+
+extern int  termdraw__initialize(termdraw__options_initialize_t options);
+
+#define     termdraw_initialize(...) termdraw__initialize((termdraw__options_initialize_t){ __VA_ARGS__ })
 extern void termdraw_destroy(void);
 
 extern int  termdraw_display(void);
@@ -36,13 +89,10 @@ extern int  termdraw_cursor_get_row(void);
 extern int  termdraw_cursor_get_col(void);
 extern int  termdraw_cursor_move(unsigned row, unsigned col);
 
-extern int  termdraw_add_rune(termdraw_rune rune);
+extern int  termdraw_add_rune(termdraw_rune_t rune);
 
-extern int  termdraw_style_reset(void);
-extern int  termdraw_style_bold(void);
-extern int  termdraw_style_italic(void);
-extern int  termdraw_style_underline(void);
-extern int  termdraw_style_strikethrough(void);
+extern int  termdraw_mode_set(unsigned mode);
+extern int  termdraw_mode_reset(void);
 
 #endif /* TERMDRAW_H_ */
 
@@ -73,15 +123,16 @@ static struct termios termdraw__original_settings = {0};
 static unsigned termdraw__cursor_row = -1;
 static unsigned termdraw__cursor_col = -1;
 
-int termdraw_initialize(void)
+int termdraw__initialize(termdraw__options_initialize_t options)
 {
+    if (tcgetattr(STDIN_FILENO, &termdraw__original_settings) == -1)
+    {
+        return -1;
+    }
+
+    if (options.raw_mode)
     {
         struct termios raw_mode_settings = {0};
-
-        if (tcgetattr(STDIN_FILENO, &termdraw__original_settings) == -1)
-        {
-            return -1;
-        }
 
         raw_mode_settings = termdraw__original_settings;
 
@@ -99,9 +150,8 @@ int termdraw_initialize(void)
     }
 
     {
-        termdraw__output_buf.data =
-            malloc(termdraw__output_buf.capacity *
-                   sizeof *termdraw__output_buf.data);
+        termdraw__output_buf.data = malloc(termdraw__output_buf.capacity *
+                                           sizeof *termdraw__output_buf.data);
 
         if (termdraw__output_buf.data == NULL)
         {
@@ -134,10 +184,9 @@ int termdraw__output_buf_append_str(const char *buf, size_t size)
             termdraw__output_buf.capacity *= 2;
         }
 
-        termdraw__output_buf.data =
-            realloc(termdraw__output_buf.data,
-                    termdraw__output_buf.capacity *
-                    sizeof(*termdraw__output_buf.data));
+        termdraw__output_buf.data = realloc(termdraw__output_buf.data,
+                                            termdraw__output_buf.capacity *
+                                            sizeof(*termdraw__output_buf.data));
 
         if (termdraw__output_buf.data == NULL)
         {
@@ -303,7 +352,7 @@ int termdraw_cursor_get_col(void)
     return termdraw__cursor_col;
 }
 
-int termdraw_add_rune(termdraw_rune rune)
+int termdraw_add_rune(termdraw_rune_t rune)
 {
     if (rune > 127)
     {
@@ -316,29 +365,22 @@ int termdraw_add_rune(termdraw_rune rune)
     }
 }
 
-int termdraw_style_reset(void)
+int termdraw_mode_reset(void)
 {
     return termdraw__output_buf_append_str("\033[0m", 4);
 }
 
-int termdraw_style_bold(void)
+/* TODO: add support for | */
+int termdraw_mode_set(unsigned mode)
 {
-    return termdraw__output_buf_append_str("\033[1m", 4);
-}
+    if (termdraw__output_buf_append_str("\033[", 2) == -1 ||
+        termdraw__output_buf_append_uint(mode)      == -1 ||
+        termdraw__output_buf_append_str("m", 1)     == -1)
+    {
+        return -1;
+    }
 
-int termdraw_style_italic(void)
-{
-    return termdraw__output_buf_append_str("\033[3m", 4);
-}
-
-int termdraw_style_underline(void)
-{
-    return termdraw__output_buf_append_str("\033[4m", 4);
-}
-
-int termdraw_style_strikethrough(void)
-{
-    return termdraw__output_buf_append_str("\033[9m", 4);
+    return 0;
 }
 
 
